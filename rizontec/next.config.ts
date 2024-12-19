4ff859d72b2e093b8601edfa7a8b53fca9568077
Gabriel Constante Schlichting <constanteg984@gmail.com>
2024-12-19 04:17:16 -0300

    oioi
    io

---
+++ rizontec/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+module.exports = {
+  experimental: {
+    turboMode: false,
+  },
+};
+export default nextConfig;
